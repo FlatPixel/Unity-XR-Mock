e5f88c1535dbd351500a11fcaac5277745a887ad
AdrienMgm <adrien.mgm@gmail.com>
2021-05-25 15:50:06 +0200

    [UP][Samples] Update default scene and sample with more realistic behaviouh and expose duration state for simulate session

--- Runtime/Simulation/SimulateSession.cs
+++ Runtime/Simulation/SimulateSession.cs
@@ -6,9 +6,11 @@ namespace FlatPixel.XR.Mock.Example
 {
     public class SimulateSession : MonoBehaviour
     {
+        [Header("Tracking")]
         [SerializeField]
         TrackingState m_TrackingState = TrackingState.None;
 
+        [Space()]
         [SerializeField]
         private List<MockTrackable> m_trackables;
 
@@ -23,7 +25,14 @@ namespace FlatPixel.XR.Mock.Example
             Finished
         }
 
-        State m_State;
+        [Header("Simulation States (handle trackable discovery)")]
+        [SerializeField]
+        State m_InnerState;
+
+        [Space()]
+        [SerializeField]
+        [Tooltip("Array of times to wait in each inner state. Index of the duration array correspond to the index of the inner state. (1 sec by default)")]
+        float[] m_DurationPerInnerState;
 
         private void Start()
         {
@@ -40,34 +49,39 @@ namespace FlatPixel.XR.Mock.Example
         {
             SessionApi.trackingState = m_TrackingState;
 
-            switch (m_State)
+            float stateDuration = 1;
+            int indexEnum = (int)m_InnerState;
+            if (m_DurationPerInnerState != null && indexEnum < m_DurationPerInnerState.Length)
+                stateDuration = m_DurationPerInnerState[indexEnum];
+
+            switch (m_InnerState)
             {
                 case State.Uninitialized:
-                    m_State = State.Initialized;
+                    m_InnerState = State.Initialized;
                     m_LastTime = Time.time;
                     break;
                 case State.Initialized:
-                    m_State = State.WaitingForFirstTrackable;
+                    m_InnerState = State.WaitingForFirstTrackable;
                     m_TrackingState = TrackingState.Limited;
                     m_LastTime = Time.time;
                     break;
                 case State.WaitingForFirstTrackable:
-                    if (Time.time - m_LastTime > 2f)
+                    if (Time.time - m_LastTime > stateDuration)
                     {
                         if (m_trackables.Count > 0)
                             m_trackables[0].gameObject.SetActive(true);
 
-                        m_State = State.WaitingToAddRemainingTrackables;
+                        m_InnerState = State.WaitingToAddRemainingTrackables;
                         m_LastTime = Time.time;
                     }
                     break;
                 case State.WaitingToAddRemainingTrackables:
-                    if (Time.time - m_LastTime > 2f)
+                    if (Time.time - m_LastTime > stateDuration)
                     {
                         for (int i = 1; i < m_trackables.Count; i++)
                             m_trackables[i].gameObject.SetActive(true);
 
-                        m_State = State.Finished;
+                        m_InnerState = State.Finished;
                         m_TrackingState = TrackingState.Tracking;
                         m_LastTime = Time.time;
                     }
